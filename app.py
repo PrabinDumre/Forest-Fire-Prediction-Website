06932dfeeb9261f9ff18fdddc358851f79f64ffa
Prabin <prabindumre16700@gmail.com>
2024-11-30 22:34:57 +0530

    added

--- app.py
+++ app.py
@@ -1,42 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template, url_for
+import pickle
 import numpy as np
-import joblib
-import os
 
 app = Flask(__name__)
 
 # Load the model
-model_path = os.path.join(os.path.dirname(__file__), 'model.joblib')
-model = joblib.load(model_path)
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
+    return render_template("forest_fire.html")
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        int_features = [int(x) for x in request.form.values()]
+        final = [np.array(int_features)]
+        prediction = model.predict_proba(final)
+        output = '{0:.{1}f}'.format(prediction[0][1], 2)
+
+        if float(output) > 0.5:
+            return render_template('forest_fire.html', 
+                                pred='It is in Danger.\nProbability of fire occurring is {}'.format(output),
+                                body_class='danger-bg')
+        else:
+            return render_template('forest_fire.html',
+                                pred='It is safe.\n Probability of fire occurring is {}'.format(output),
+                                body_class='safe-bg')
     return render_template('forest_fire.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        # Get values from the form
-        oxygen = float(request.form['Oxygen'])
-        temperature = float(request.form['Temperature'])
-        humidity = float(request.form['Humidity'])
-        
-        # Make prediction
-        features = np.array([[oxygen, temperature, humidity]])
-        prediction = model.predict(features)
-        
-        # Format the prediction message
-        if prediction[0] == 1:
-            pred = 'It is in Danger. Probability of fire occurring is 1.00'
-            body_class = 'danger-bg'
-        else:
-            pred = 'It is Safe. Probability of fire occurring is 0.00'
-            body_class = 'safe-bg'
-            
-        return render_template('forest_fire.html', pred=pred, body_class=body_class)
-        
-    except Exception as e:
-        return render_template('forest_fire.html', pred='Error: Invalid input values')
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
