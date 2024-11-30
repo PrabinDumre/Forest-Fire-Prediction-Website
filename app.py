9f0944d14cc5b7dbf37b8dbd98edbf3b4bebd6fd
Prabin <prabindumre16700@gmail.com>
2024-11-30 22:26:32 +0530

    added

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request
 import numpy as np
-import pickle
+import joblib
 import os
 
 app = Flask(__name__)
 
 # Load the model
-model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
-model = pickle.load(open(model_path, 'rb'))
+model_path = os.path.join(os.path.dirname(__file__), 'model.joblib')
+model = joblib.load(model_path)
 
 @app.route('/')
 def home():
@@ -22,7 +22,8 @@ def predict():
         humidity = float(request.form['Humidity'])
         
         # Make prediction
-        prediction = model.predict([[oxygen, temperature, humidity]])
+        features = np.array([[oxygen, temperature, humidity]])
+        prediction = model.predict(features)
         
         # Format the prediction message
         if prediction[0] == 1:
@@ -37,6 +38,5 @@ def predict():
     except Exception as e:
         return render_template('forest_fire.html', pred='Error: Invalid input values')
 
-# Add this for Vercel
 if __name__ == '__main__':
     app.run()
